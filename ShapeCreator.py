226cc9c67be7a8bb547b98e6603d33b16a48de6d
Potter <marcypotter16@gmail.com>
2023-04-27 12:58:15 +0200

    pro

---
+++ ShapeCreator.py
@@ -0,0 +1,13 @@
+import pymunk as pm
+import pygame as pg
+
+
+class ShapeCreator:
+    def __init__(self):
+        pass
+
+    def update(self, dt: float):
+        pass
+
+    def render(self, surface: pg.Surface):
+        pass
